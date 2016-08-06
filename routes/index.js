209811de96eb35a8e990008159cfbbdfca8ee574
백지훈 <bjh970913@gmail.com>
2016-08-06 11:41:55 +0900

    create paths

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,17 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* POST home page. */
+router.post('/create', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.post('/join', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.post('/update', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
 module.exports = router;
