3da3273143d6c9fbf4c99c7537b34f16f93365a8
백지훈 <bjh970913@gmail.com>
2016-08-06 11:53:39 +0900

    no message

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
+var MongoClient = require('mongodb').MongoClient;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/mongo', function(req, res, next) {
+  res.write('mongo test');
 });
 
 /* POST home page. */
